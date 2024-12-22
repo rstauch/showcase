48dcdeb5669520946119ad7ff93562d4622e2ec8
Stauch, R. (Robert) <robert.stauch@fluxdev.de>
2024-12-22 18:12:12 +0100

    Add Spring-Boot to Service module

---
+++ service/src/test/java/com/rs/ContextLoadsTest.java
@@ -0,0 +1,11 @@
+package com.rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextLoadsTest {
+
+  @Test
+  void contextLoads() {}
+}
