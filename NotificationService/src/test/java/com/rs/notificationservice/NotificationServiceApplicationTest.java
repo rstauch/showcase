59e460296e5ae934aa08c982daa1af5f86477f96
rstauch <70838812+rstauch@users.noreply.github.com>
2024-12-29 13:10:18 +0100

    New Module structure (#8)
    * cleanup
    
    * NotificationService module

---
+++ NotificationService/src/test/java/com/rs/notificationservice/NotificationServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.rs.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTest {
+
+  @Test
+  void contextLoads() {
+    // make sure spring boot context loads
+  }
+}
